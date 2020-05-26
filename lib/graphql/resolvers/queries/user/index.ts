55fd6c786099b21860d9c41ad4e118edc3d15ab0
Osama Adil <adilosama47@gmail.com>
2020-05-26 15:57:42 +0200

    throw an error if user isn't found in user query

--- lib/graphql/resolvers/queries/user/index.ts
+++ lib/graphql/resolvers/queries/user/index.ts
@@ -1,10 +1,15 @@
+import { ApolloError } from 'apollo-server-fastify';
 import { UserQuery, UsersQuery } from './index.d';
 
 export default {
 	user: (async (_, { where }, { models }, __) => {
 		const { User } = models;
 
-		return await User.query().findOne({ ...where });
+		const user = await User.query().findOne({ ...where });
+
+		if (!user) throw new ApolloError('User not found!');
+
+		return user;
 	}) as UserQuery,
 	users: (async (_, { where, query }, { models }, __) => {
 		const { User } = models;
