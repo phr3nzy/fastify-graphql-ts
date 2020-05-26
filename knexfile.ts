fd407e7fec4e7c3dfef7e2e96bf88d3968a77fc5
Osama Adil <adilosama47@gmail.com>
2020-05-26 11:45:31 +0200

    add logger instance for migrations

--- knexfile.ts
+++ knexfile.ts
@@ -1,5 +1,6 @@
 import { Config } from 'knex';
 import config from './lib/config';
+import { logger } from './lib/utils/logger';
 
 const { DATABASE_URL, LOG_LEVEL } = config;
 
@@ -8,6 +9,12 @@ const knexMigrationsConfig: Config = {
 	connection: DATABASE_URL,
 	migrations: {
 		directory: './lib/db/migrations/**/*.migration.ts',
+	},
+	log: {
+		warn: logger.warn,
+		debug: logger.debug,
+		error: logger.error,
+		enableColors: true,
 	},
 	debug: LOG_LEVEL === 'debug',
 };
